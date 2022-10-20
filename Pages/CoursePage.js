b7ab942842f2e32498ced8bbd284c09a4acf09fb
GUEST-1001 <nawatgame@live.com>
2022-10-21 04:10:19 +0700

    ADD CoursePage.js //Count down  can use

--- Pages/CoursePage.js
+++ Pages/CoursePage.js
@@ -1,28 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect } from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 
-let date = new Date();
-let endHours = date.getHours();
-let endMinutes = date.getMinutes();
-let endSeconds = date.getSeconds();
+import styles from "../StylesSheets/stryles";
 
-const CoursePage = () => {
-    
-    useEffect(() => {
-        endHours = date.getHours();
-        endMinutes = date.getMinutes();
-        endSeconds = date.getSeconds();
-      }, [date.getSeconds()]);
+const CoursePage = ({ navigation }) => {
+  const [timePass, setTimePass] = useState(true);
+
+  const [timeLeft, setTimeLeft] = useState(null);
+  const [targetTime, setTargetTime] = useState(5000);
+  let resendTimerInterval;
+
+  const CountTimeLeft = (finalTime) => {
+    const difference = finalTime - +new Date();
+    if (difference >= 0) {
+      setTimeLeft(Math.round(difference / 1000));
+    } else {
+        setTimeLeft(Math.round(difference / 1000));
+        clearInterval(resendTimerInterval)
+        navigation.navigate("MainPage");
+    }
+  };
+
+  const TiggerTime = () => {
+    console.log(+new Date())
+    const finalTime = +new Date() + targetTime;
+    resendTimerInterval = setInterval(() => {
+        CountTimeLeft(finalTime) , 1000
+    })
+  };
+
+  useEffect(() => {
+    TiggerTime();
+  }, []);
+
+  // setTimeout(() => {
+  //     setTimePass(false);
+  // }, 5000);
 
   return (
     <View>
-      <Text>
-        {endHours}:{endMinutes}:{endSeconds}
-      </Text>
+      <Text>{timeLeft}</Text>
     </View>
   );
 };
 
 export default CoursePage;
-
-const styles = StyleSheet.create({});
